f78aef7db439f91409899bd79038f4b15f2961ba
Bonaventure-Wachira <wachira.bonaventure@students.jkuat.ac.ke>
2023-04-22 02:32:25 +0000

    ain

---
+++ Project_3/values.h
@@ -0,0 +1,13 @@
+
+
+// This file contains function definitions for the evaluation functions
+
+typedef char* CharPtr;
+enum Operators {LESS, GREATER, LESS_EQUAL, GREATER_EQUAL, EQUAL, NOT_EQUAL, ADD, SUBTRACT, MULTIPLY, DIVIDE, MODULUS, AND, OR};
+
+
+
+int evaluateReduction(Operators operator_, int head, int tail);
+int evaluateRelational(int left, Operators operator_, int right);
+int evaluateArithmetic(int left, Operators operator_, int right);
+
